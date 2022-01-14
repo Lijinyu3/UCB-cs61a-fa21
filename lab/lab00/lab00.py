aaf67c0dacdd50611b63a585a3cfd1896aed3380
Lijinyu3 <enotaa33@gmail.com>
2022-01-14 20:36:01 +0800

    Finished lab00

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_one():
+    # The lines in the triple-quotes """ are called a docstring, 
+    # which is a description of what the function is supposed to do. 
+    # When writing code in 61A, you should always read the docstring!
+    """Come up with the most creative expression that evaluates to 2021,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_one()
+    2021
+    """
+    return 2021 + 0*(8243923824 + 3241412)
