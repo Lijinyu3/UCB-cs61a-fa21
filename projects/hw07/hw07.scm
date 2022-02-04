a77de071644b678bf65ab4440118e4212922711e
Lijinyu3 <enotaa33@gmail.com>
2022-02-04 23:44:46 +0800

    Finished hw07

--- projects/hw07/hw07.scm
+++ projects/hw07/hw07.scm
@@ -1,11 +1,21 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (ordered? s) 'YOUR-CODE-HERE)
+(define (ordered? s)
+  (if (null? (cdr s))
+      #t
+      (if (< (cadr s) (car s))
+          #f
+          (ordered? (cdr s)))))
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= exp 1)
+      base
+      (if (even? exp)
+          (square (pow base (/ exp 2)))
+          (* base (pow base (- exp 1))))))
