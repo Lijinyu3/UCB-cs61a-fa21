c83f0562c87af424e2543a4e4100a1d59e635566
Lijinyu3 <enotaa33@gmail.com>
2022-02-04 21:54:44 +0800

    Finished lab10

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,31 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  ;   (cond 
+  ;     ((< num1 num2) -1)
+  ;     ((= num1 num2) 0)
+  ;     (else          1)))
+  (if (< num1 num2)
+      -1
+      (if (= num1 num2)
+          0
+          1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (x) (+ num x)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define lst 'YOUR-CODE-HERE)
+; (define lst (list (list 1) 2 (list 3 4) 5))
+; (define lst (list '(1) 2 '(3 4) 5))
+; (define lst '((1) 2 (3 4) 5))
+(define lst
+        (cons (cons 1 nil)
+              (cons 2 (cons (cons 3 (cons 4 nil)) (cons 5 nil)))))
 
-(define (remove item lst) 'YOUR-CODE-HERE)
+; (define (remove item lst)
+;   (cond
+;     ((null? lst)
+;      lst)
+;     ((= (car lst) item)
+;      (remove item (cdr lst)))
+;     (else
+;      (cons (car lst) (remove item (cdr lst))))))
+(define (remove item lst) (filter (lambda (i) (not (= i item))) lst))
