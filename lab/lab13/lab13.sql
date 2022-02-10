35b063971f24a86d4f4dceccf51cb63581d0868d
Lijinyu3 <enotaa33@gmail.com>
2022-02-11 01:19:31 +0800

    Finished lab13

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,24 +2,36 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet
+    FROM students
+    WHERE color = "blue" AND pet = "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song
+    FROM students
+    WHERE color = "blue" AND pet = "dog";
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest
+    FROM students
+    GROUP BY smallest
+    HAVING COUNT(*) = 1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a_stu.pet, a_stu.song, a_stu.color, b_stu.color
+    FROM students as a_stu, students as b_stu
+    WHERE a_stu.pet = b_stu.pet AND a_stu.song = b_stu.song AND a_stu.time < b_stu.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven
+    FROM students, numbers
+    WHERE students.time = numbers.time AND number = 7 AND numbers.'7' = "True";
 
 
 CREATE TABLE avg_difference AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT ROUND(AVG(ABS(number - smallest)))
+    FROM students;
 
